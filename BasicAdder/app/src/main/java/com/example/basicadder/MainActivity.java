f75a61ff7bc8107c2cb20e25a5b916106f2f804b
Glenn Lopez <glennlopez@gmail.com>
2019-03-08 14:43:32 -0600

    updated to be functional

--- BasicAdder/app/src/main/java/com/example/basicadder/MainActivity.java
+++ BasicAdder/app/src/main/java/com/example/basicadder/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.basicadder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //my code - tut: https://www.youtube.com/watch?v=aJAS3r-OvYQ&t=1925s
+        Button addbtn = (Button) findViewById(R.id.addbtn);
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.ResultTextView);
+
+                // do the math
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+
+                resultTextView.setText(result + "");
+            }
+        });
     }
 }
