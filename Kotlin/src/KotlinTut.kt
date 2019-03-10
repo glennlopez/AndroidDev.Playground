98b6422411115030c18960b12d1e68e4fe8b347c
Glenn Lopez <glennlopez@gmail.com>
2019-03-09 22:26:42 -0600

    update

--- Kotlin/src/KotlinTut.kt
+++ Kotlin/src/KotlinTut.kt
@@ -78,8 +78,46 @@ fun main(): Unit {
         var myString2 = "This is another random thing"
         println("Second Index: ${myString1[2]}")
         println("Index 16 - 22: ${myString2.subSequence(16, 22)}")
+        println("myString1 contains \"random\": ${myString1.contains("random")} ")
+        println("myString1 contains \"poop\": ${myString1.contains("poop")} ")
+        println()
 
 
 
+
+    /*
+     * Arrays: https://youtu.be/H_oGi8uuDpA?t=1246
+     */
+        var myArray = arrayOf(1, 2, "Lopez", "Glenn", "B")
+        myArray[4] = "A+"
+        println("Arrays: ${myArray[0]} ${myArray[1]} ${myArray[2]} ${myArray[3]} ${myArray[4]}")
+        println("Arrays: ${myArray.asList()}")
+        println("Contains \"Glenn\": ${myArray.contains("Glenn")}")
+        println("Contains \"glenn\": ${myArray.contains("glenn")}")
+        println("Size of array: ${myArray.size}")
+        println("First array: ${myArray.first()}")
+        println("Last array: ${myArray.last()}")
+        println("Index of \"Lopez\": ${myArray.indexOf("Lopez")}")
+
+        // build an array ful of squares of size 5
+        var sqArray = Array(20, { x -> x * x } )
+        println("10^10: ${sqArray[10]}")
+        println("Array of squares: " + sqArray.asList())
+
+        // type specific array
+        var intArray: Array<Int> = arrayOf(1,2,3) // try and add a char or string
+        println("Int specific array: ${intArray.asList()}")
+
+
+    /*
+     * Ranges: https://youtu.be/H_oGi8uuDpA?t=1576
+     */
+        var oneToTen = 1..10
+        var tenToOne = 10.downTo(1)
+        var sevenToThirty = se
+        var alphabet = "A".."Z"
+        println("oneToTen: ${oneToTen.toList()}")
+        println("tenToOne: " + tenToOne.toList())
+        println("G is in alphabet range: ${"G" in alphabet}")
 
 }
